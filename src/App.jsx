fc56fd8181ecfa67ba093bb9b7814246432919c0
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-02-25 17:35:54 -0500

    iterando array en react

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,9 @@ function App() {
         pacientes={pacientes}
         setPacientes={setPacientes}
       />
-      <ListadoPacientes/>
+      <ListadoPacientes
+        pacientes={pacientes}
+      />
 
       </div>
     </div>
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,7 @@
 import Pacientes from "./Pacientes";
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
+
     return ( 
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
@@ -8,11 +9,13 @@ const ListadoPacientes = () => {
                 Administra tus Pacientes {''}
                 <span className="text-indigo-600 font-bold">y Citas</span>
             </p>
-            <Pacientes/>
-            <Pacientes/>
-            <Pacientes/>
-            <Pacientes/>
-            <Pacientes/>
+
+            {pacientes.map( paciente =>(  
+                    <Pacientes
+                        paciente={paciente}
+                    />
+                )
+            )}
         </div>
      );
 }
