ec196228427f3f1c44a05d9c1699c4a9cefa06b2
JoelC24 <libra-marriaga@hotmail.com>
2022-02-27 01:26:17 -0500

    Elimina correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
 
+  const eliminarPaciente = (id) =>{
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+
+    setPacientes(pacientesActualizados)
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header
@@ -24,6 +30,7 @@ function App() {
       <ListadoPacientes
         pacientes={pacientes}
         setPaciente={setPaciente}
+        eliminarPaciente={eliminarPaciente}
       />
 
       </div>
--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,7 +1,7 @@
-const Pacientes = ({paciente, setPaciente}) => {
+const Pacientes = ({paciente, setPaciente, eliminarPaciente}) => {
     
     
-const {nombre, propietario, email, fecha, sintomas} = paciente
+const {nombre, propietario, email, fecha, sintomas, id} = paciente
 return ( 
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
                 <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -35,6 +35,7 @@ return (
                     <button
                         type="button"
                         className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                        onClick={()=> eliminarPaciente(id)}
                     >
                         Eliminar
                     </button>
