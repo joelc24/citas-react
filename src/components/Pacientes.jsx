e94d8fc114b98c31f3b64002f5a57229155f20c6
JoelC24 <libra-marriaga@hotmail.com>
2022-02-26 21:40:33 -0500

    llenando formulario con useEffect

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,6 +1,8 @@
 const Pacientes = ({paciente, setPaciente}) => {
-        const {nombre, propietario, email, fecha, sintomas} = paciente
-    return ( 
+    
+    
+const {nombre, propietario, email, fecha, sintomas} = paciente
+return ( 
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
                 <p className="font-bold mb-3 text-gray-700 uppercase">
                     Nombre: {''}
